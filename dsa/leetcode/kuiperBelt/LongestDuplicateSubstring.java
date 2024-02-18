dc262be61c821e91c79f2760b93ede6b31f2aa7d
everrover <30935508+everrover@users.noreply.github.com>
2024-02-18 18:07:56 +0530

    added robin-karp algo notes

--- dsa/leetcode/kuiperBelt/LongestDuplicateSubstring.java
+++ dsa/leetcode/kuiperBelt/LongestDuplicateSubstring.java
@@ -61,6 +61,27 @@ public class LongestDuplicateSubstring {
    * This solution utilizes Rabin-Karp algorithm for efficient substring search.
    * It performs binary search on substring length(the entire solution space) to find the longest duplicating substring.
    * 
+   * ROBIN-KARP ALGORITHM:
+   * 1. It is a string searching algorithm that uses hashing to find any one of a set of pattern strings in a text.
+   * 2. It is a probabilistic algorithm that uses hashing to find any one of a set of pattern strings in a text.
+   * 
+   * For a string of length n, the algorithm can find a pattern of length m in O(n+m) time. The pattern is hashed and 
+   * compared with the hash of the substring of the same length. 
+   * 
+   * Theoretically, for str[0,m-1] initial hash is calculated and then for str[1,m] the hash is calculated using the previous hash:
+   * 
+   * initial-hash(str[0,m-1]) = str[0] + prime * str[1] + prime^2 * str[2] + ... + prime^(m-1) * str[m-1]
+   * new-hash(str[i,i+m-1]) = (hash - str[i-1]) + prime^(m-1) * str[i+m-1]
+   * 
+   * Although for current problem, I used which is much more optimal:
+   * 
+   * initial-hash(str[0,m-1]) = str[m-1] + prime * str[m-2] + prime^2 * str[m-3] + ... + prime^(m-1) * str[0]
+   * new-hash(str[i,i+m-1]) = (hash - str[i] * prime^(m-1)) * prime + str[i+m-1]
+   * 
+   * HASH COLLISIONS:
+   * 1. The algorithm uses a prime number to reduce the chance of hash collisions. 
+   * 2. If hash is found to exist, the algorithm compares the patterns matching the hash with the substring to avoid false positives.
+   * 
    * TC: O(n log n) SC: O(n)
    * #binary-search #sliding-window #rolling-hash #suffix-array #hard #binary-search-over-solution-space
    */
