c4b0623648fdcf465cea842b620dfea682a54286
everrover <30935508+everrover@users.noreply.github.com>
2024-07-17 01:09:29 +0530

    added 2 pointer solution

--- dsa/leetcode/kuiperBelt/MinimumWindowSubsequence.java
+++ dsa/leetcode/kuiperBelt/MinimumWindowSubsequence.java
@@ -48,8 +48,37 @@ class MinimumWindowSubsequence {
     }
     return dp[i][j]=res;
   }
-}
 
 /*
-Recursive DFS with memoization to find minimum window substring length.
+Two pointer approach:
 */
+  public String minWindowSlidingWindow(String s1, String s2) {
+    int st = 0, minLength = Integer.MAX_VALUE, start=0, end=0;
+    while(st<s1.length()){
+      int r1 = st, r2 = 0;
+      while(r1<s1.length()){
+        if(s1.charAt(r1) == s2.charAt(r2)){
+          r2++;
+          if(s2.length() == r2) break;
+        }
+        r1++;
+      }
+      if(r2 != s2.length()) break; // no subseq found
+      int l1=r1, l2=s2.length()-1;
+      while(l2>=0){
+        if(s1.charAt(l1) == s2.charAt(l2)){
+          l2--;
+          if(s2.length() == l2) break;
+        }
+        l1--;
+      }
+      if(r1-l1+1<minLength){
+        start = l1;
+        end = r1+1;
+        minLength = r1-l1+1;
+      }
+      st = l1+1;
+    }
+    return s1.substring(start, end);
+  }
+}
