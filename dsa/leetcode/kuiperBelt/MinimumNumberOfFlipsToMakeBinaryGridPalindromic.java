5914930fb89e0c49f2ccec8f2f33fb06f6aadf30
everrover <30935508+everrover@users.noreply.github.com>
2024-08-03 18:39:21 +0000

    Added basic desc : MinimumNumberOfFlipsToMakeBinaryGridPalindromic.java for LC3239

---
+++ dsa/leetcode/kuiperBelt/MinimumNumberOfFlipsToMakeBinaryGridPalindromic.java
@@ -0,0 +1,35 @@
+package dsa.leetcode.kuiperBelt;
+
+// imports here (if necessary)
+
+public class MinimumNumberOfFlipsToMakeBinaryGridPalindromic {
+  /**
+   * https://leetcode.com/problems/minimum-number-of-flips-to-make-binary-grid-palindromic-i/
+   *
+   * The algorithm counts the number of mismatched elements in rows and columns by comparing symmetric elements
+   * and finds the minimum flips required to make either all rows or all columns palindromic.
+   *
+   * TC: O(m * n/2)
+   * SC: O(1)
+   * #grid #matrix #palindrome #greedy #medium
+   */
+
+  public int minFlips(int[][] grid) {
+    final int m = grid.length, n = grid[0].length;
+    // check row transitions
+    int cnt = 0;
+    for(int i=0; i<m; i++){
+      for(int j=0; j<n/2; j++){
+        if(grid[i][j] != grid[i][n-j-1]) cnt++;
+      }
+    }
+    // check col transitions
+    int res = cnt; cnt = 0;
+    for(int i=0; i<m/2; i++){
+      for(int j=0; j<n; j++){
+        if(grid[i][j] != grid[m-i-1][j]) cnt++;
+      }
+    }
+    return Math.min(res, cnt);
+  }
+}
