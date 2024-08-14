93282e0f6079f6342194795f136176cb743527b6
everrover <30935508+everrover@users.noreply.github.com>
2024-08-14 18:09:23 +0530

    ch

--- dsa/leetcode/kuiperBelt/MaxPointsOnALine.java
+++ dsa/leetcode/kuiperBelt/MaxPointsOnALine.java
@@ -8,8 +8,16 @@ public class MaxPointsOnALine {
     /**
      * https://leetcode.com/problems/max-points-on-a-line/
      * 
+     * Used 3-nested iteration to calculate slope and intercept for each pair of points.
+     * Then checked the count of points which satisfy slope and intercept for the count.
+     * It's TC is O(n^3) and it fails for large inputs. Especially with double values.
      * 
-     * Iterate over each point and calculate slope for each other point. Use a hashmap to store slopes.
+     * y = mx + c
+     * Then thought of two point line formulae to calculate slope m = (y2-y1)/(x2-x1) and c = y1 - m*x1
+     * 
+     * Iterate over each point and calculate slope for each other point. This considers slope and constant automatically
+     * while computing the slope. Also, intercept need not be calculated or hashed.
+     * 
      * The key of hashmap will be slope and value will be the frequency of each slope.
      * Finally, return the maximum value from the hashmap.
      * 
--- dsa/leetcode/kuiperBelt/MaximumSumBstInBinaryTree.java
+++ dsa/leetcode/kuiperBelt/MaximumSumBstInBinaryTree.java
@@ -51,4 +51,78 @@ public class MaximumSumBstInBinaryTree {
         int max = Math.max(root.val, right[1]);  // find the maximum value in the current subtree
         return new int[]{min, max, sum};         // return the parameters of the current subtree
     }
+
+    /*
+    class Solution {
+  private static class T{
+    public int val, sum;
+    public Integer large, small;
+    public boolean isBst;
+    public T left, right;
+    public T(int val, int sum, boolean isBst){
+      this.val = val;
+      this.small = this.large = null;
+      this.sum = sum;
+      this.isBst = isBst;
+    }
+    public T(int val){ // single node
+      this.val = this.sum = this.small = this.large = val;
+      this.isBst = true;
+    }
+  }
+  public int maxSumBST(TreeNode root) {
+    T newr = traversal(root);
+    int res = findMax(newr);
+    return res;
+  }
+
+  private T traversal(TreeNode root) {
+    if(root == null) return null;
+    T l = traversal(root.left);
+    T r = traversal(root.right);
+    if(l == null && r == null) return new T(root.val);
+    T curr = new T(root.val);
+    
+    boolean isBst = false;
+    Integer large = null, small = null;
+    if(l == null){
+      small = curr.val;
+      isBst = true;
+    } else {
+      curr.sum += l.sum;
+      if(l.isBst) {
+        isBst = l.large<root.val;
+        small = l.small;
+      }else{
+        isBst = false;
+      }
+    }
+    if(r == null){
+      large = curr.val;
+      isBst = isBst && true;
+    }else{
+      curr.sum += r.sum;
+      if(isBst && r.isBst) {
+        isBst = r.small>root.val;
+        large = r.large;
+      }else{
+        isBst = false;
+      }
+    }
+    if(isBst) {curr.large = large; curr.small = small;}
+    else {curr.large = curr.small = null;}
+    curr.isBst = isBst;
+    curr.left = l; curr.right = r;
+    return curr;
+  }
+  
+  private int findMax(T root){
+    if(root==null) return 0;
+    int max = root.isBst?root.sum:0;
+    max = Math.max(max, findMax(root.left));
+    max = Math.max(max, findMax(root.right));
+    return max;
+  }
+}
+     */
 }
