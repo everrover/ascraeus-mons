33b0e644e44a7adda508897eb4b969dc59fa121d
everrover <30935508+everrover@users.noreply.github.com>
2024-08-27 09:56:51 +0530

    review commits

--- dsa/leetcode/jovianMoonOrbit/SwimInRisingWater.java
+++ dsa/leetcode/jovianMoonOrbit/SwimInRisingWater.java
@@ -12,6 +12,10 @@ public class SwimInRisingWater {
    * Keep a min-heap to always expand the smallest possible current time.
    * Pop the smallest element in the heap, and push all its unvisited neighbors into the heap.
    * Repeat until we reach the bottom-right cell.
+   * 
+   * I saw people using binary search over solution space and apply BFS/Dijkstra's algorithm to check if it's possible to reach the end cell within the given time.
+   * 
+   * How is this problem even hard?
    *
    * TC: O(n^2 log n) SC: O(n^2)
    * #array #binary-search #dfs #bfs #union-find #heap #matrix #hard
@@ -35,16 +39,49 @@ public class SwimInRisingWater {
       T t = pq.poll();
       res = Math.max(res, t.ht);
       mark[t.x][t.y] = true;
-      // check and push left neighbor
       if (t.y > 0 && !mark[t.x][t.y - 1]) pq.offer(new T(t.x, t.y - 1, grid[t.x][t.y - 1]));
-      // check and push right neighbor
       if (t.y < m - 1 && !mark[t.x][t.y + 1]) pq.offer(new T(t.x, t.y + 1, grid[t.x][t.y + 1]));
-      // check and push upper neighbor
       if (t.x > 0 && !mark[t.x - 1][t.y]) pq.offer(new T(t.x - 1, t.y, grid[t.x - 1][t.y]));
-      // check and push bottom neighbor
       if (t.x < n - 1 && !mark[t.x + 1][t.y]) pq.offer(new T(t.x + 1, t.y, grid[t.x + 1][t.y]));
       if (t.x == m - 1 && t.y == n - 1) break;
     }
     return res;
   }
+
+  /**
+   * // Binary search solution - CPP
+   * class Solution {
+    public:
+        int swimInWater(vector<vector<int>>& grid) {
+            int n = grid.size();
+            int low = grid[0][0], hi = n*n-1;
+            while (low < hi) {
+                int mid = low + (hi-low)/2;
+                if (valid(grid, mid)) 
+                  hi = mid;
+                else
+                  low = mid+1;
+            }
+            return low;
+        }
+    private:
+        bool valid(vector<vector<int>>& grid, int waterHeight) {
+            int n = grid.size();
+            vector<vector<int>> visited(n, vector<int>(n, 0));
+            vector<int> dir({-1, 0, 1, 0, -1});
+            return dfs(grid, visited, dir, waterHeight, 0, 0, n);
+        }
+        bool dfs(vector<vector<int>>& grid, vector<vector<int>>& visited, vector<int>& dir, int waterHeight, int row, int col, int n) {
+            visited[row][col] = 1;
+            for (int i = 0; i < 4; ++i) {
+                int r = row + dir[i], c = col + dir[i+1];
+                if (r >= 0 && r < n && c >= 0 && c < n && visited[r][c] == 0 && grid[r][c] <= waterHeight) {
+                    if (r == n-1 && c == n-1) return true;
+                    if (dfs(grid, visited, dir, waterHeight, r, c, n)) return true;
+                }
+            }
+            return false;
+        }
+    };
+   */
 }
