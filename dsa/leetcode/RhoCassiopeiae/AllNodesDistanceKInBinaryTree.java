b97e0e6e6130071010da53d2b669f97bcbf91518
everrover <30935508+everrover@users.noreply.github.com>
2024-11-28 19:26:35 +0530

    added custom notes

--- dsa/leetcode/RhoCassiopeiae/AllNodesDistanceKInBinaryTree.java
+++ dsa/leetcode/RhoCassiopeiae/AllNodesDistanceKInBinaryTree.java
@@ -5,11 +5,15 @@ import java.util.*;
 /**
  * https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
  *
- * Traverse the tree starting from the target node. Use DFS to calculate depths and determine nodes
- * at distance K using backtracking approach on parent paths if needed.
+ * It's simple post-order traversal with depth tracking. If `right` subtree has target node,
+ * then we do the same traversal on `left` subtree to add resulting nodes. Vice-versa case is handled by default.
+ * In worst case the tree would be traversed twice, no more.
+ * 
+ * I also thought of some solutions where a graph was constructed from the tree and then BFS/DFS was applied
+ * to find the nodes at distance `k`. I was able to do the same with just the post-order tree traversal.
  *
  * TC: O(n) SC: O(n)
- * #binary-tree #tree #dfs #bfs #medium
+ * #binary-tree #tree #dfs #bfs #binary-traversals #bfs #medium
  */
 
 class TreeNode {
@@ -22,7 +26,7 @@ class TreeNode {
 class Solution {
     List<Integer> res = new LinkedList<>();
     private int k;
-    private int dfs(TreeNode root, TreeNode target, boolean ptfound, int ptdepth) {
+    private int postOrder(TreeNode root, TreeNode target, boolean ptfound, int ptdepth) {
         if (root == null) return -1;
 
         int tdepth = -1;
@@ -37,17 +41,17 @@ class Solution {
             else if (ptdepth > k) return -1;
         }
 
-        int ltdepth = dfs(root.left, target, ptfound, ptdepth + (ptfound ? 1 : 0));
+        int ltdepth = postOrder(root.left, target, ptfound, ptdepth + (ptfound ? 1 : 0));
         if (ltdepth != -1) {
             tdepth = Math.max(tdepth, 1 + ltdepth);
             if (ltdepth == k) res.add(root.val);
-            dfs(root.right, target, true, 1 + ltdepth);
+            postOrder(root.right, target, true, 1 + ltdepth);
         } else {
-            int rtdepth = dfs(root.right, target, ptfound, ptdepth + (ptfound ? 1 : 0));
+            int rtdepth = postOrder(root.right, target, ptfound, ptdepth + (ptfound ? 1 : 0));
             if (rtdepth != -1) {
                 if (rtdepth == k) res.add(root.val);
                 tdepth = Math.max(tdepth, 1 + rtdepth);
-                dfs(root.left, target, true, 1 + rtdepth);
+                postOrder(root.left, target, true, 1 + rtdepth);
             }
         }
 
@@ -56,7 +60,7 @@ class Solution {
 
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         this.k = k;
-        dfs(root, target, false, 0);
+        postOrder(root, target, false, 0);
         return res;
     }
 }
--- dsa/leetcode/RhoCassiopeiae/AmountOfTimeForBinaryTreeToBeInfected.java
+++ dsa/leetcode/RhoCassiopeiae/AmountOfTimeForBinaryTreeToBeInfected.java
@@ -3,12 +3,14 @@ package dsa.leetcode.RhoCassiopeiae;
 /**
  * https://leetcode.com/problems/amount-of-time-for-binary-tree-to-be-infected/
  *
+ * Another possible solution:
  * Convert the tree to an undirected graph to make it easier to handle.
- * Use BFS starting at the start node to find the distance between each node and the start node.
- * The answer is the maximum distance.
+ * Use DFS/BFS starting at the start node to find maximum distance from the start node to any other node
+ * 
+ * ❗️ Needed help at it... for optimal post-order traversal solution
  *
  * TC: O(n), SC: O(n)
- * #tree #graph #bfs #medium
+ * #tree #graph #bfs #medium #post-order-traversal #dfs
  */
 
 public class AmountOfTimeForBinaryTreeToBeInfected {
@@ -37,13 +39,15 @@ public class AmountOfTimeForBinaryTreeToBeInfected {
       int leftdepth = traverse(root.left, start);
       int rightdepth = traverse(root.right, start);
       int depth = 0;
-      if (root.val == start) {
+      if (root.val == start) { // one possible solution, max depth of the tree under infected node
         depth = -1;
         maxDist = Math.max(leftdepth, rightdepth);
-      } else if (leftdepth >= 0 && rightdepth >= 0) { // all nodes connected
+      } else if (leftdepth >= 0 && rightdepth >= 0) { // all nodes below aren't infected
         depth = Math.max(leftdepth, rightdepth) + 1;
-      } else {
-        // Calculate max distance
+      } else { // one of the nodes is infected 
+        // - -ve values are the depth of the infected node
+        // - combined with the other subtree, we get the max distance
+        // ./AmountOfTimeForBinaryTreeToBeInfected.jpeg
         maxDist = Math.max(maxDist, Math.abs(leftdepth) + Math.abs(rightdepth));
         depth = Math.min(leftdepth, rightdepth) - 1;
       }
