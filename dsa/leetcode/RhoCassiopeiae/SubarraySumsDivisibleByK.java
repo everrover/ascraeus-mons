eb6a287dc3d7f1db09610d4a7210f54d97bf5296
everrover <30935508+everrover@users.noreply.github.com>
2024-12-11 20:17:26 +0530

    added sol

--- dsa/leetcode/RhoCassiopeiae/SubarraySumsDivisibleByK.java
+++ dsa/leetcode/RhoCassiopeiae/SubarraySumsDivisibleByK.java
@@ -2,14 +2,16 @@ package dsa.leetcode.RhoCassiopeiae;
 
 import java.util.*;
 
-public class Solution {
+public class SubarraySumsDivisibleByK {
   /**
    * https://leetcode.com/problems/subarray-sums-divisible-by-k/
    *
-   * Find the number of non-empty subarrays with a sum divisible by k.
-   * Calculate the running prefix sum and track the remainder modulo k.
-   * Use a hashmap to count the number of times each remainder occurs.
-   * If the remainder is seen before, add its count to the result.
+   * if sum(i,j) is divisible by k, then sum(0,j) - sum(0,i-1) is divisible by k
+   * => (prefix_j - prefix_(i-1)) % k == 0
+   * => prefix_j % k == prefix_(i-1) % k
+   * 
+   * so we hash the prefix-sum % k and count the occurences of the same prefix sum and those
+   * can be used to form subarrays divisible by k.
    *
    * TC: O(n) SC: O(k)
    * #array #hash-table #prefix-sum #medium
@@ -20,7 +22,7 @@ public class Solution {
     map.put(0, 1);
     int prefix = 0, res = 0;
     for(int num: nums) {
-      prefix = (prefix + num % k + k) % k;
+      prefix = (prefix + num % k + k) % k; // handle negative modulus with num % k + k
       if(map.containsKey(prefix)) res += map.get(prefix);
       map.put(prefix, map.getOrDefault(prefix, 0) + 1);
     }
