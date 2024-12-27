43b0f62b4b95bba74a6d3d880c50877e8c2feb53
Abhishek Deyol <30935508+everrover@users.noreply.github.com>
2024-12-27 08:51:12 +0530

    Added solution as would be discussed in an interview

--- dsa/leetcode/fermi_s_paradox/ShortestSubarrayWithSumAtLeastK.java
+++ dsa/leetcode/fermi_s_paradox/ShortestSubarrayWithSumAtLeastK.java
@@ -11,8 +11,12 @@ public class ShortestSubarrayWithSumAtLeastK {
    * For each index `i`  need to find the smallest sum and an largest index j such
    * that prefix_i - prefix_j >= k. We can use a monotonic queue to keep track of 
    * the indices of the prefix sums. 
-   * Also, we can use monotonic increasing stack with binary-search for the same. It works 
-   * in logaritmic time, though.
+   * Came up to this via sourcing monotonic increasing stack with binary-search for the same. It works 
+   * in logaritmic time, though. 
+
+   * How? When performing binary search, the indexes that were found as a part of solution for `i` were all going from bottom of the stack to the top in a linear order. Simply meaning the bottom most entries were not required for next iterations. And so came up with double ended monotonic queue as mentioned above.
+   * 
+   * ❗I came up with all my building insights one on top of another. Brute force O(n^3) -> Brute force with prefix sum array O(n^2) -> Priority queue O(n * logn) -> Monotonic stack and binary search -> Double ended Monotonic queue O(n)
    *
    * TC: O(n), where n is the length of the array.
    * SC: O(n), to store the prefix sums and deque.
@@ -62,7 +66,8 @@ public class ShortestSubarrayWithSumAtLeastK {
 
       // basically, we try to find the largest index j such that rsum - q[j].s >= k
       // by tracking smallest running sums, we can iterate over largest arrays that support our req
-      // they won't be used for next iterations, so we can remove them
+      // they won't be used for next iterations, so we can remove them 
+      // ❗ think in terms of positive and negative values added to commutative sum `rsum` and it's difference with `k`
       while (!q.isEmpty() && rsum-q.peek().s >= k) {
         res = Math.min(res,
                       i-q.poll().idx);
