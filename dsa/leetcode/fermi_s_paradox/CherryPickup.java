f2750af74a8785a6145786386f1e05f803d0d82a
everrover <30935508+everrover@users.noreply.github.com>
2024-12-28 01:06:50 +0530

    added mods

--- dsa/leetcode/fermi_s_paradox/CherryPickup.java
+++ dsa/leetcode/fermi_s_paradox/CherryPickup.java
@@ -7,7 +7,10 @@ public class CherryPickup {
   /**
    * https://leetcode.com/problems/cherry-pickup/
    *
-   * To maximize cherry collection, we simulate the movement from (0,0) to (n-1,n-1) and back using dynamic programming with memoization. The key is to keep track of two paths moving as a pair and utilize the grid's state for optimal cherry picking, avoiding thorns. This problem is solved using a 3D dynamic programming approach to store states.
+   * To maximize cherry collection, we simulate the movement from (0,0) to (n-1,n-1) and 
+   * back considering two people going the same way but possibly on different paths.
+   * 
+   * r1 + c1 = r2 + c2, since both would cover the same distance.
    *
    * TC: O(n^3) SC: O(n^3)
    * #array #dynamic-programming #matrix #hard
@@ -16,7 +19,6 @@ public class CherryPickup {
   class Solution {
     private int N, M;
     private static final int MIN = Integer.MIN_VALUE;
-    private static final int MIN2 = -1000000;
 
     public int cherryPickup(int[][] grid) {
       N = grid.length; M = grid[0].length;
@@ -27,20 +29,21 @@ public class CherryPickup {
 
     private int dfs(int r1, int c1, int c2, final int[][]grid, final int [][][]dp){
       // we can either go right or down / left or bottom,
-      // if we know three other one can be computed
+      // if we know three other one can be computed - saves memo space and time
       int r2 = r1+c1-c2;
       // if we've reached the end or we've reached a wall
-      if(r1 == N || r2 == N || c1 == N || c2 == N || grid[r1][c1] == -1 || grid[r2][c2] == -1) return MIN2;
+      if(r1 == N || r2 == N || c1 == N || c2 == N || grid[r1][c1] == -1 || grid[r2][c2] == -1) return MIN;
       else if(r1 == N-1 && c1 == M-1) return grid[r1][c1];
       else if(dp[r1][c1][c2] != MIN) return dp[r1][c1][c2];
       int res = grid[r1][c1];
-      if(c1 != c2) res += grid[r2][c2]; // cherry would already be picked up
+      if(c1 != c2) res += grid[r2][c2]; // if diff cols, diff locations would be there for p1 and p2
+      // else same location for both
       res += Math.max(
-        Math.max(
+        Math.max( // same row diff col for p2
           dfs(r1, c1+1, c2+1, grid, dp),
           dfs(r1+1, c1, c2+1, grid, dp)
         ),
-        Math.max(
+        Math.max( // same col diff row for p2
           dfs(r1, c1+1, c2, grid, dp),
           dfs(r1+1, c1, c2, grid, dp)
         )
