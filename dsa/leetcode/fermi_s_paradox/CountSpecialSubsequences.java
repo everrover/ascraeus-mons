b84d805cc868313467cd04ed1f0171ec832eec3b
everrover <30935508+everrover@users.noreply.github.com>
2024-12-29 14:16:56 +0530

    added mods

--- dsa/leetcode/fermi_s_paradox/CountSpecialSubsequences.java
+++ dsa/leetcode/fermi_s_paradox/CountSpecialSubsequences.java
@@ -7,9 +7,34 @@ public class CountSpecialSubsequences {
 
   /**
    * https://leetcode.com/problems/count-special-subsequences/
-   *
+   * 
    * Count pairs where nums[p] / nums[q] equals nums[s] / nums[r],
    * using GCD to handle ratios efficiently.
+   * 
+   * Earlier thought of storing these fractions in a 2D map linked to 
+   * the list of indexes where they occur. And apply binary search to find
+   * the count of fractions that match the current fraction.
+   * for(int p=0; i<nums.length; i++){
+   *  for(int q=i+2; j<nums.length; j++){
+   *   int []f = gcdfactors(nums[i], nums[j]);
+   *   if(map[f[0]][f[1]] != null){ map[f[0]][f[1]].add(j); }
+   *   else{ map[f[0]][f[1]] = new ArrayList<>(); map[f[0]][f[1]].add(j); }
+   *  }
+   * }
+   * // sort all the lists in the map
+   * for(int i=nums.length-1; i>=0; i--){
+   *  for(int j=i-2; j>=0; j--){
+   *   int []f = gcdfactors(nums[i], nums[j]);
+   *   if(map[f[0]][f[1]] != null){
+   *    int idx = Collections.binarySearch(map[f[0]][f[1]], j-2);
+   *    if(idx >= 0){
+   *     res += map[f[0]][f[1]].size() - idx;
+   *    }
+   *   }
+   *  }
+   * }
+   * 
+   * But, it's O(n^2 log n) and not efficient as the approach mentioned below.
    *
    * TC: O(n^2) SC: O(n^2)
    * #array #mathematical #medium #math #gcd
@@ -35,8 +60,11 @@ public class CountSpecialSubsequences {
     long res = 0;
     int [][]map = new int[1001][1001];
     int p=0, q=0, r=0, s=0;
+    // why starting from r?
+    // because for a given r, we can find all the pairs of p,q that can be formed
+    // came to me at the last moment
     for(r=0; r<nums.length; r++){
-      for(; q<r-1; q++){
+      for(; q<r-1; q++){ // for a given p,q pair we don't need to recompute the fractions again
         for(p=0; p<q-1; p++){
           int []f = gcdfactors(nums[p], nums[q]) ;
           map[f[0]][f[1]]++;
--- dsa/leetcode/fermi_s_paradox/NumberOfWaysToFormTargetStringGivenDictionary.java
+++ dsa/leetcode/fermi_s_paradox/NumberOfWaysToFormTargetStringGivenDictionary.java
@@ -2,38 +2,43 @@ package dsa.leetcode.fermi_s_paradox;
 
 import java.util.*;
 
-public class Solution {
+public class NumberOfWaysToFormTargetStringGivenDictionary {
   /**
    * https://leetcode.com/problems/number-of-ways-to-form-a-target-string-given-a-dictionary/?envType=daily-question&envId=2024-12-29
    *
-   * Use frequency array to track available characters and dynamic programming to form target string.
+   * Use frequency array to track available characters. Once a character is used, it's position can't
+   * be used again. So, we either skip or use the current character for the target string.
    * 
-   * TC: O(n*m*t), where n is the length of words, m is the word length, and t is the target length.
-   * SC: O(m*t)
+   * TC: O(m*t), m is the word length[ALL WORDS ARE SAME], and t is the target length.
+   * SC: O(m*t + m*m)
    * #dynamic-programming #string #hard
    */
   
-  private static final int MOD = (int)1e9 + 7;
+  private static int MOD = (int)1e9+7;
+  private int dfs(int i, int j, final int [][]words, final String target, final int [][]dp){
+    if(j == target.length()) return 1;
+    else if(i == words.length) return 0;
+    else if(dp[i][j] != -1) return dp[i][j];
 
+    // don't include the current character for the target string
+    // since i+1 if used won't allow use of i again we don't iterate for all words
+    long cnt = dfs(i+1, j, words, target, dp)%MOD;
+
+    // include the current character for the target string
+    if(words[i][target.charAt(j)-'a'] > 0) cnt = (cnt + 1L * words[i][target.charAt(j)-'a'] * dfs(i+1, j+1, words, target, dp))%MOD;
+
+    return dp[i][j] = (int)cnt;
+  }
   public int numWays(String[] words, String target) {
     final int M = words[0].length();
-    int[][] dp = new int[M + 1][target.length() + 1];
-    for (int[] row : dp) Arrays.fill(row, -1);
-    return dfs(0, 0, words, target, dp);
-  }
-
-  private int dfs(int i, int j, final String[] words, final String target, final int[][] dp) {
-    if (j == target.length()) return 1;
-    if (i == words[0].length()) return 0;
-    if (dp[i][j] != -1) return dp[i][j];
-
-    long cnt = dfs(i + 1, j, words, target, dp) % MOD;
-    for (String word : words) {
-      if (word.charAt(i) == target.charAt(j)) {
-        cnt = (cnt + 1L * dfs(i + 1, j + 1, words, target, dp)) % MOD;
+    int [][]wa = new int[M][26];
+    for(String word: words){
+      for(int i=0; i<M; i++){
+        wa[i][word.charAt(i)-'a']++;
       }
     }
-
-    return dp[i][j] = (int) cnt;
+    int [][]dp = new int[M][target.length()];
+    for(int []dd: dp) Arrays.fill(dd, -1);
+    return dfs(0,0,wa,target,dp);
   }
 }
