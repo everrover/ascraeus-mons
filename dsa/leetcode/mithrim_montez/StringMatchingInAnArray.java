cc55c2916fc81167c7a2789c646f20f6df61a38a
everrover <30935508+everrover@users.noreply.github.com>
2025-01-07 15:34:45 +0000

    Added basic desc : StringMatchingInAnArray.java for LC1408

---
+++ dsa/leetcode/mithrim_montez/StringMatchingInAnArray.java
@@ -0,0 +1,28 @@
+package dsa.leetcode.mithrim_montez;
+
+import java.util.*;
+
+public class StringMatchingInAnArray {
+  /**
+   * https://leetcode.com/problems/string-matching-in-an-array/description/?envType=daily-question&envId=2025-01-07
+   *
+   * Simply iterate each word and check if it is a substring of any other word in the list.
+   * Add it to the result list if it is found as a substring.
+   *
+   * TC: O(n^2 * m) SC: O(n)
+   * #array #string #string-matching #easy
+   */
+  public List<String> stringMatching(String[] words) {
+    int n = words.length;
+    List<String> ans = new ArrayList<>();
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (i != j && words[j].contains(words[i])) {
+          ans.add(words[i]);
+          break;
+        }
+      }
+    }
+    return ans;
+  }
+}
