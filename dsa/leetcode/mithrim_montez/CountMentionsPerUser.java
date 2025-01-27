0200c93408883f105f495d7bda1f8d0717f101c4
everrover <30935508+everrover@users.noreply.github.com>
2025-01-27 22:59:38 +0530

    added the solution

--- dsa/leetcode/mithrim_montez/CountMentionsPerUser.java
+++ dsa/leetcode/mithrim_montez/CountMentionsPerUser.java
@@ -8,31 +8,44 @@ public class CountMentionsPerUser {
    * https://leetcode.com/problems/count-mentions-per-user/description/
    *
    * Sort events by timestamp then process each event.
-   * Maintain two sets for offline and online users IDs and calculate mentions.
+   * Maintain a set of offline users and uses it to update mentions
+   * 
+   * Simple event simulation/B.Force
    *
    * TC: O(n log n) SC: O(n)
    * #array #set #event-simulation #medium
    */
 
-  public int[] countMentions(int numberOfUsers, List<List<String>> events) {
-    int[] res = new int[numberOfUsers];
-    int[] offline = new int[numberOfUsers];
-    Arrays.fill(offline, -1);
-
-    Collections.sort(events, (a, b) -> {
-      int A = Integer.valueOf(a.get(1));
+   public int[] countMentions(int numberOfUsers, List<List<String>> events) {
+    int []res = new int[numberOfUsers];
+    Collections.sort(events, (a,b)->{
       int B = Integer.valueOf(b.get(1));
-      if (A == B) {
+      int A = Integer.valueOf(a.get(1));
+      if(A==B) {
         return b.get(0).compareTo(a.get(0));
       }
-      return A - B;
+      return A-B;
     });
-
-    for (List<String> eve : events) {
-      // Event processing logic goes here
-      // Handle MESSAGE and OFFLINE events
+    int []offline = new int[numberOfUsers];
+    Arrays.fill(offline, -1);
+    for(List<String> eve: events){
+      // update online
+      int ts = Integer.valueOf(eve.get(1));
+      for(int i=0; i<offline.length; i++){
+        if(offline[i] != -1 && ts - offline[i] >= 60) offline[i] = -1;
+      }
+      if(eve.get(0).equals("MESSAGE")){
+        if(eve.get(2).equals("ALL")){
+          for(int i=0; i<numberOfUsers; i++) res[i]++;
+        }else if(eve.get(2).equals("HERE")) {
+          for(int i=0; i<numberOfUsers; i++) if(offline[i] == -1) res[i]++;
+        }else for(String id: eve.get(2).split(" ")){
+          res[Integer.valueOf(id.substring(2))]++;
+        }
+      }else{
+        offline[Integer.valueOf(eve.get(2))] = ts;
+      }
     }
-
     return res;
   }
 }
--- dsa/leetcode/mithrim_montez/MaximumFrequencyAfterSubarrayOperation.java
+++ dsa/leetcode/mithrim_montez/MaximumFrequencyAfterSubarrayOperation.java
@@ -1,32 +1,63 @@
 package dsa.leetcode.mithrim_montez;
 
 public class MaximumFrequencyAfterSubarrayOperation {
-
-    /**
-     * https://leetcode.com/problems/maximum-frequency-after-subarray-operation/description/
-     *
-     * To find the maximum frequency of the value k after the operation, first fix the element you want to convert to k.
-     * Then, use prefix sums to optimize counting occurrences of an element.
-     *
-     * TC: O(n) SC: O(1)
-     * #prefix-sum #sliding-window #medium
-     */
-
-    public int maxFrequencyAfterOperation(int[] nums, int k) {
-        int res = 0, currcnt = 0;
-        for(int num: nums){
-            if(num == k) currcnt++;
-            else currcnt--;
-            currcnt = Math.max(currcnt, 0);
-            res = Math.max(currcnt, res);
-        }
-        // Example for tracing operation
-        // // y y y y y x x k x x x k k x x k y y y y
-        // Return: maximum frequency difference
-        return res;
+/**
+ * https://leetcode.com/problems/maximum-frequency-after-subarray-operation/description/
+ *
+ * We're basically finding the majority element amongst a `num` and the element `k`.
+ * For each subarray, we've got three cases:
+ * 3 parts
+ * [0,x] ... (0) [x+1, y] ... (1) [y+1, n-1] ... (2)
+ * = ks[x] + (nums[y]-nums[x]) + (ks[n-1]-ks[y])
+ * = ks[x]+nums[y]-nums[x]+ks[n-1]-ks[y]
+ * = nums[y] - nums[x] - (ks[y] - ks[x]) ... (I)
+ *   + ks[n-1] ... (II)
+ * 
+ * Doesn't yeild anything.
+ * 
+ * ...(I) - For any given subarray, a `num` and a `k`, if we encounter `num`(+1) else if we 
+ * encounter `k`(-1). This essentially represents simulates this sum. Any `num`'s out of this subarray 
+ * are assumed to be 0 and all `k`'s are assumed to be 1. So, we essentially need to 
+ * find the max-sum subarray of such 1's and -1's. Brute force doesn't work, so we use 
+ * Kadane's algorithm to find the max-sum subarray. Since `num` is limited to 50, we can 
+ * do it for each `num` and find the max sum subarray across all `num`'s. 
+ * 
+ * `num`+`x`=`k` => `k`+`x`!=`k` if `x`!=0
+ * +1 is added because if we add `x` to the subarray which transforms it to a `k` our `res` increases by 1
+ * -1 is added if we add `k` to the subarray which transforms sth other than `k` and `res` decreases by 1
+ * 
+ * - To get the result we add the count of `k`'s in the original array due to ...(II).
+ *
+ * TC: O(n) SC: O(1)
+ * #prefix-sum #sliding-window #medium #tricky
+ */
+  
+ public int maxFrequency(int[] nums, int k) {
+    int []nummap = new int[51];
+    for(int num: nums) nummap[num]++;
+    int res = 0;
+    for(int i=0; i<=50; i++){
+      res = Math.max(res, kadanesearch(nums, k, i));
     }
+    return res+nummap[k];
+  }
 
-    private int kadanesearch(final int[] nums, final int k, final int b) {
-        return res + nummap[k];
+  private int kadanesearch(final int []nums, final int k, final int b) {
+    int res = 0, currcnt = 0;
+    for(int num: nums){
+      if(num == b) currcnt++;
+      if(num == k) currcnt--;
+      currcnt = Math.max(currcnt, 0);
+      res = Math.max(currcnt, res);
     }
+    // y y y y y x x k x x x k k x x k y y y y
+    // return 7-4=3 for x
+    return res;
+  }
 }
+// k=2
+// 1,1,1,1,2,2,2,1,2,3,3,3,2,3,2,3,3
+// 1 2 3 4 4 4 4 5 5 5 5 5 5 5 5 5 5
+// 0 0 0 0 1 2 3 3 4 4 4 4 5 5 6 6 6
+// 0 0 0 0 0 0 0 0 0 1 2 3 3 4 4 5 6
+// 
