fc8662ba1347e5de79d251dce61381aee67a8c94
everrover <30935508+everrover@users.noreply.github.com>
2025-02-15 23:03:44 +0530

    added mods

--- dsa/leetcode/VallesMarineris/ProductOfTheLastKNumbers.java
+++ dsa/leetcode/VallesMarineris/ProductOfTheLastKNumbers.java
@@ -8,7 +8,8 @@ import java.util.List;
  *
  * Solution:
  * Keep all prefix products of numbers in an array, then calculate the product of last K elements in O(1) complexity.
- * When a zero number is added, clean the array of prefix products.
+ * When a zero number is added, clean the array of prefix products. I am using a zeromark to keep track of the last zero.
+ * And set the prefix product to 1 for the next number.
  * 
  * TC: O(1) for both add and getProduct methods
  * SC: O(n)
@@ -17,43 +18,36 @@ import java.util.List;
 
 public class ProductOfTheLastKNumbers {
     private List<Long> pre;
-    private int zeromark;
-
+    private int n, zeromark = -1;
     public ProductOfTheLastKNumbers() {
-        this.pre = new ArrayList<>();
-        this.pre.add(1L);
-        this.zeromark = -1;
+      this.pre = new ArrayList<>(1000);
+      pre.add(1L);
+      this.n = 1;
     }
-
+    
     public void add(int num) {
-        long res = num == 0 ? 0L : num * this.pre.get(this.pre.size() - 1);
-        if (num == 0) {
-            this.zeromark = this.pre.size();
-            this.pre.add(1L);
-        } else {
-            this.pre.add(res);
-        }
+      long res = 0L;
+      if(num == 0){
+        res = 1L;
+        this.zeromark = n;
+      }else{
+        res = num*pre.get(n-1);
+      }
+      this.pre.add(res);
+      this.n++;
     }
-
+    
     public int getProduct(int k) {
-        int n = this.pre.size();
-        if (this.zeromark >= n - k) {
-            return 0;
-        }
-        return (int)(this.pre.get(n - 1) / this.pre.get(n - k - 1));
+      if(n-k-1 < zeromark) return 0;
+      // System.out.println(k+":"+pre+":"+pre.get(n-k-1));
+      int res = (int)(pre.get(n-1)/pre.get(n-k-1));
+      return res;
     }
-
-    public static void main(String[] args) {
-        ProductOfTheLastKNumbers productOfNumbers = new ProductOfTheLastKNumbers();
-        productOfNumbers.add(3);
-        productOfNumbers.add(0);
-        productOfNumbers.add(2);
-        productOfNumbers.add(5);
-        productOfNumbers.add(4);
-        System.out.println(productOfNumbers.getProduct(2)); // 20
-        System.out.println(productOfNumbers.getProduct(3)); // 40
-        System.out.println(productOfNumbers.getProduct(4)); // 0
-        productOfNumbers.add(8);
-        System.out.println(productOfNumbers.getProduct(2)); // 32
-    }
-}
+  }
+  
+  /**
+   * Your ProductOfNumbers object will be instantiated and called as such:
+   * ProductOfNumbers obj = new ProductOfNumbers();
+   * obj.add(num);
+   * int param_2 = obj.getProduct(k);
+   */
--- dsa/leetcode/VallesMarineris/SeparateSquaresI.java
+++ dsa/leetcode/VallesMarineris/SeparateSquaresI.java
@@ -5,26 +5,44 @@ public class SeparateSquaresI {
   /**
    * https://leetcode.com/problems/separate-squares-i/description/
    *
-   * The task is to find a horizontal line such that the total area of squares above the line equals the total area of squares below the line.
-   * Binary search can be applied over the y-coordinates to find the minimum y-value satisfying this condition.
+   * What i got stuck on was type-casting the result of the area calculation to double.
+   * 
+   * Otherwise it's a binary-search problem over solution space. Where the line we sweep is the y-axis
+   * and is our solution space.
    *
    * TC: O(n log(maxY)) SC: O(1)
    * #binary-search #geometry #medium
    */
 
-  private double findAreaBelow(double ydx, int [][]squares){
-    double aa = 0.0, ab = 0.0;
-    for(int []sq: squares){
-      if(sq[1] >= ydx) {
-        aa += (double)sq[2]*(double)sq[2];
-      } else if(sq[1]+sq[2] <= ydx) {
-        ab += (double)sq[2]*(double)sq[2];
-      } else {
-        aa += (double)sq[2]*((double)sq[1]+(double)sq[2]-ydx);
-        ab += (double)sq[2]*(ydx-(double)sq[1]);
-      }
-    }
-    return (low+high)/2.0;
-  }
+   final double eps = 10e-7;
+   public double separateSquares(int[][] squares) {
+     double high = 0.0, low = 0.0;
+     for(int []sq: squares) {
+       high = Math.max(high, sq[1]+sq[2]);
+       low = Math.min(low, sq[1]);
+     }
+     while(Math.abs(high-low) > eps){
+       double mid = (low+high)/2;
+       double areadiff = findAreaBelow(mid, squares);
+       if(areadiff > 0) low  = mid;
+       else high = mid;
+     }
+     return (low+high)/2.0;
+   }
+ 
+   private double findAreaBelow(double ydx, int [][]squares){
+     double aa = 0.0, ab = 0.0;
+     for(int []sq: squares){
+       if(sq[1] >= ydx) {
+         aa += (double)sq[2]*(double)sq[2];
+       } else if(sq[1]+sq[2] <= ydx) {
+         ab += (double)sq[2]*(double)sq[2];
+       } else {
+         aa += (double)sq[2]*((double)sq[1]+(double)sq[2]-ydx);
+         ab += (double)sq[2]*(ydx-(double)sq[1]);
+       }
+     }
+     return aa-ab;
+   }
 
 }
