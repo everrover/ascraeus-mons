00c609f0618199434d9848a7219b1c25c23b8f0f
everrover <30935508+everrover@users.noreply.github.com>
2025-02-16 23:56:34 +0530

    added mods

--- dsa/leetcode/VallesMarineris/ConstructTheLexicographicallyLargestValidSequence.java
+++ dsa/leetcode/VallesMarineris/ConstructTheLexicographicallyLargestValidSequence.java
@@ -8,38 +8,68 @@ public class ConstructTheLexicographicallyLargestValidSequence {
    * This solution uses backtracking to construct the sequence by placing integers from n to 1 
    * at possible positions ensuring that the distance between two occurrences of an integer i
    * is exactly i. The sequence is constructed in such a way to ensure it is lexicographically largest.
+   * 
+   * Earlier used a version that used counts and checked for i's presence at `idx-1` if `cnt[i] == 1 
+   * // i.e it was used before once`.
+   * Didn't work.
    *
    * TC: O(n!) SC: O(n)
    * #array #backtracking #medium
    */
 
-  public int[] constructSequence(int n) {
-    int[] res = new int[2 * n - 1];
-    boolean[] cnt = new boolean[n + 1];
-
-    findPerm(n, 0, cnt, res);
-    return res;
-  }
-
-  private boolean findPerm(final int n, int idx, boolean[] cnt, int[] res) {
-    while (idx < res.length && res[idx] != 0) idx++;
-    if (idx == res.length) return true;
-
-    for (int i = n; i >= 1; i--) {
-      if (i != 1) {
-        if (cnt[i]) continue;
-        if (idx + i >= res.length || res[idx + i] != 0) continue;
-        res[idx] = res[idx + i] = i;
+  // private static int count = 0;
+  private boolean findPerm(final int n, int idx, boolean []cnt, int []res){
+    while(idx < res.length && res[idx] != 0) idx++;
+    if(idx == res.length) return true;
+    // count++;
+    for(int i=n; i >= 1; i--){
+      if(cnt[i]) continue;
+      if(i!=1){
+        if(idx + i >= res.length || res[idx+i] != 0) continue;
+        cnt[i]=true;
+        res[idx] = res[idx+i] = i;
+        if(findPerm(n, idx+1, cnt, res)) return true;
+        res[idx] = res[idx+i] = 0;
+        cnt[i]=false;
+      }else{
+        res[idx] = 1;
         cnt[i] = true;
-        if (findPerm(n, idx + 1, cnt, res)) return true;
+        if(findPerm(n, idx+1, cnt, res)) return true;
         cnt[i] = false;
-        res[idx] = res[idx + i] = 0;
-      } else {
-        res[idx] = i;
-        if (findPerm(n, idx + 1, cnt, res)) return true;
         res[idx] = 0;
       }
     }
     return false;
   }
+  public int[] constructDistancedSequence(final int n) {
+    int []res = new int[2*n-1];
+    boolean []cnt = new boolean[n+1];
+    findPerm(n, 0, cnt, res);
+    // System.out.println(count);
+    return res;
+  }
+
+  // check at prev version
+  // private boolean findPerm(final int n, int idx, int []cnt, int []res){
+  //   if(idx == res.length) return true;
+  //   for(int i=n; i >= 1; i--){
+  //     if(cnt[i] == 0) continue;
+  //     if(i!=1){
+  //       if(cnt[i] == 1 && (idx < i || res[idx-i] != i)) continue;
+  //     }
+  //     cnt[i]--;
+  //     res[idx] = i;
+  //     if(findPerm(n, idx+1, cnt, res)) return true;
+  //     res[idx] = 0;
+  //     cnt[i]++;
+  //   }
+  //   return false;
+  // }
+  // public int[] constructDistancedSequence(final int n) {
+  //   int []res = new int[2*n-1];
+  //   int []cnt = new int[n+1];
+  //   Arrays.fill(cnt, 2); cnt[1] = 1;
+  //   findPerm(n, 0, cnt, res);
+  //   return res;
+  // }
 }
