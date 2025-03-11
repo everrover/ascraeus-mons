8099b4272bcad1776e9377886d404d6b58ffd72d
everrover <30935508+everrover@users.noreply.github.com>
2025-03-11 21:10:19 +0530

    added mods

--- dsa/leetcode/VallesMarineris/CountOfSubstringsContainingEveryVowelAndKConsonantsIi.java
+++ dsa/leetcode/VallesMarineris/CountOfSubstringsContainingEveryVowelAndKConsonantsIi.java
@@ -14,47 +14,54 @@ public class CountOfSubstringsContainingEveryVowelAndKConsonantsIi {
    * #sliding-window #string #hash-table #medium
    */
 
-  public long countSubstringsWithAllVowels(String word, int k) {
-    long count = 0, res = 0;
-    Map<Character, Integer> vowmap = new HashMap<>();
-    Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+  private static Set<Character> vowels = new HashSet<>();
+  public long countOfSubstrings(String word, int k) {
+    if(vowels.isEmpty()) {
+      vowels.add('a');
+      vowels.add('e');
+      vowels.add('i');
+      vowels.add('o');
+      vowels.add('u');
+    }
+    char []chs = word.toCharArray();
     int l = 0, r = 0, consonants = 0;
-    char[] chs = word.toCharArray();
-    while (r < chs.length) {
+    Map<Character, Integer> vowmap = new HashMap<>();
+    long count = 0, res = 0;
+    while(r<chs.length){
       consonants = add(chs[r], vowmap, consonants);
-      while (l <= r && consonants > k) {
+      while(l<=r && consonants > k) {
+        count = 0; // the count of leading vowels is reset here since only when consonants are removed
+        // we start a new range of substrings
         consonants = rem(chs[l], vowmap, consonants);
         l++;
       }
-      if (vowmap.size() == 5) {
-        // Every vowel is present
-        res += count + 1;
+      if(consonants == k && vowmap.size()==5) {
+        while(consonants == k && l<=r) {
+          if(vowels.contains(chs[l]) && vowmap.get(chs[l]) > 1) vowmap.put(chs[l], vowmap.get(chs[l])-1);
+          else break;
+          l++; count++;
+        }
+        res += (count+1);
       }
       r++;
     }
     return res;
   }
 
-  private int add(char c, Map<Character, Integer> vowmap, int consonants) {
-    if (vowmap.containsKey(c)) {
-      vowmap.put(c, vowmap.get(c) + 1);
-    } else {
-      if ('a' <= c && c <= 'z' && !vowmap.containsKey(c)) {
-        cps
-      }
+  private int add(char ch, Map<Character, Integer> vowmap, int consonants){
+    if(vowels.contains(ch)) {
+      vowmap.put(ch, vowmap.getOrDefault(ch, 0)+1);
+    }else{
+      consonants++;
     }
     return consonants;
   }
 
-  private int rem(char c, Map<Character, Integer> vowmap, int consonants) {
-    if (vowmap.containsKey(c)) {
-      int count = vowmap.get(c);
-      if (count == 1) {
-        vowmap.remove(c);
-      } else {
-        vowmap.put(c, count - 1);
-      }
-    } else {
+  private int rem(char ch, Map<Character, Integer> vowmap, int consonants){
+    if(vowels.contains(ch)) {
+      vowmap.put(ch, vowmap.get(ch)-1);
+      if(vowmap.get(ch) == 0) vowmap.remove(ch);
+    }else{
       consonants--;
     }
     return consonants;
--- dsa/leetcode/VallesMarineris/DesignHashSet.java
+++ dsa/leetcode/VallesMarineris/DesignHashSet.java
@@ -1,19 +1,30 @@
 package dsa.leetcode.VallesMarineris;
 
-public class MyHashSet {
+import java.util.Arrays;
+
+public class DesignHashSet {
   private int[] tb;
 
   /**
    * https://leetcode.com/problems/design-hashset/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
    *
    * The implemented hash set uses an array where each index corresponds to a potential key, with simple operations for add, remove, and check existence.
+   * 
+   * Classic textbook problem. 
+   * 
+   * The original hash collision resolution techniques
+   * - Using List of LinkedLists and hashes to index the list. In java: List<LinkedList<Integer>>
+   * - Using a balanced binary search tree to store the elements at hash index. In java: List<TreeSet<Integer>>
+   * - Using a chained list. In java: List<List<Integer>>
+   * 
+   * Add and remove elements in case of collision needs to seek the actual element before the ops
    *
    * TC: O(1) for add, remove, and contains
    * SC: O(1) since the array is fixed in size and operations do not depend on the number of stored elements
    * #array #hash-table #design #easy
    */
 
-  public MyHashSet() {
+  public DesignHashSet() {
     tb = new int[1000001];
     Arrays.fill(tb, -1); // Initialize array with -1 to represent empty slots
   }
