21e240d029c24d7ab3df50d5408220cd3169644a
everrover <30935508+everrover@users.noreply.github.com>
2025-03-30 11:34:58 +0000

    Added basic desc : MinimumCostToReachEveryPosition.java for LC3502

---
+++ dsa/leetcode/VallesMarineris/MinimumCostToReachEveryPosition.java
@@ -0,0 +1,28 @@
+package dsa.leetcode.VallesMarineris;
+
+public class MinimumCostToReachEveryPosition {
+
+  /**
+   * https://leetcode.com/problems/minimum-cost-to-reach-every-position/description/
+   *
+   * Use a greedy approach to keep track of minimum cost encountered from the end of the array.
+   * Update the result array with this minimum cost to allow free swaps later.
+   *
+   * TC: O(n) SC: O(n)
+   * #greedy #array #minimum-cost #easy
+   */
+
+  public int[] minCosts(int[] cost) {
+    int[] res = new int[cost.length];
+    int idx = cost.length - 1;
+    int mini = cost[cost.length - 1];
+
+    while (idx >= 0) {
+      mini = Math.min(mini, cost[idx]); // update minimum cost encountered
+      res[idx] = mini;  // set the minimum cost to the current position
+      idx--;
+    }
+
+    return res;
+  }
+}
