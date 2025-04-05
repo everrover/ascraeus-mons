000d702aad849cfb23b12bea5a48798b852a5dd2
everrover <30935508+everrover@users.noreply.github.com>
2025-04-05 12:06:19 +0530

    added mods

--- dsa/leetcode/VallesMarineris/MaxNumberOfPointsFromGridQueries.java
+++ dsa/leetcode/VallesMarineris/MaxNumberOfPointsFromGridQueries.java
@@ -5,6 +5,7 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 // https://leetcode.com/problems/maximum-number-of-points-from-grid-queries/description/?envType=daily-question&envId=2025-04-05
+
 // Given an m x n integer matrix grid and an array queries of size k, determine the maximum
 // number of points obtainable by starting from the top left cell. Use BFS to gather points
 // efficiently, considering point values as a threshold for exploration.
@@ -13,55 +14,40 @@ import java.util.Queue;
 // #array #bfs #priority-queue #matrix #hard
 
 public class MaxNumberOfPointsFromGridQueries {
-  public int[] maxPoints(int[][] grid, int[] queries) {
-    int qres = 0;
-    int[][] qs = new int[queries.length][2];
-    for(int i = 0; i < qs.length; i++) {
-      qs[i][0] = queries[i];
-      qs[i][1] = i;
-    }
-    Arrays.sort(qs, (a, b) -> a[0] - b[0]);
-
-    int[] res = new int[queries.length];
-    Queue<N> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
-    pq.offer(new N(grid[0][0], 0, 0));
-    final int m = grid.length, n = grid[0].length;
-    boolean[][] v = new boolean[m][n];
-    v[0][0] = true;
-
-    for (int[] q : qs) {
-      while (!pq.isEmpty() && pq.peek().val < q[0]) {
-        N node = pq.poll();
-        qres++;
-
-        if (node.y < n - 1 && !v[node.x][node.y + 1]) {
-          pq.offer(new N(grid[node.x][node.y + 1], node.x, node.y + 1));
-          v[node.x][node.y + 1] = true;
-        }
-        if (node.x > 0 && !v[node.x - 1][node.y]) {
-          pq.offer(new N(grid[node.x - 1][node.y], node.x - 1, node.y));
-          v[node.x - 1][node.y] = true;
-        }
-        if (node.y > 0 && !v[node.x][node.y - 1]) {
-          pq.offer(new N(grid[node.x][node.y - 1], node.x, node.y - 1));
-          v[node.x][node.y - 1] = true;
-        }
-        if (node.x < m - 1 && !v[node.x + 1][node.y]) {
-          pq.offer(new N(grid[node.x + 1][node.y], node.x + 1, node.y));
-          v[node.x + 1][node.y] = true;
-        }
-      }
-      res[q[1]] = qres;
-    }
-    return res;
-  }
-
   private static class N {
-    int val, x, y;
-    N(int val, int x, int y) {
+    public int val, x, y;
+    public N(int val, int x, int y){
       this.val = val;
       this.x = x;
       this.y = y;
     }
+  }
+  public int[] maxPoints(int[][] grid, int[] queries) {
+    Queue<N> pq = new PriorityQueue<>((a,b)->a.val-b.val);
+    final int m = grid.length, n = grid[0].length;
+    int qres = 0;
+    int [][]qs = new int[queries.length][2];
+    for(int i=0; i<qs.length; i++){
+      qs[i][0] = queries[i];
+      qs[i][1] = i;
+    }
+    int []res = new int[queries.length];
+    Arrays.sort(qs, (a,b)->a[0]-b[0]);
+    pq.offer(new N(grid[0][0], 0, 0));
+    boolean v[][] = new boolean[m][n];
+    for(int []q: qs){
+      while(!pq.isEmpty() && pq.peek().val<q[0]){
+        N node = pq.poll();
+        if(v[node.x][node.y]) continue;
+        v[node.x][node.y] = true;
+        qres++;
+        if(node.x < m-1 && !v[node.x+1][node.y]) pq.offer(new N(grid[node.x+1][node.y], node.x+1, node.y));
+        if(node.y < n-1 && !v[node.x][node.y+1]) pq.offer(new N(grid[node.x][node.y+1], node.x, node.y+1));
+        if(node.x > 0 && !v[node.x-1][node.y]) pq.offer(new N(grid[node.x-1][node.y], node.x-1, node.y));
+        if(node.y > 0 && !v[node.x][node.y-1]) pq.offer(new N(grid[node.x][node.y-1], node.x, node.y-1));
+      }
+      res[q[1]] = qres;
+    }
+    return res;
   }
 }
--- dsa/leetcode/VallesMarineris/SumOfAllSubsetXORTotals.java
+++ dsa/leetcode/VallesMarineris/SumOfAllSubsetXORTotals.java
@@ -7,12 +7,23 @@ public class SumOfAllSubsetXORTotals {
    * 
    * To find the sum of all XOR totals for every subset, use depth-first search (DFS) to iterate over each element. At each element, choose to include or exclude it in the subset, calculating the XOR recursively with or without the element.
    * 
+   * Cheeky way:
+   * If we add x with x, in bits it moves left by 1 shift.
+   * If we xor n elements, and if all its subsets are added to xor, then the xor of all subsets will shift left by the count of numbers we have.
+   * I tried it with 4, 7, 11, 11. Worked.
+   * 
    * TC: O(2^n) SC: O(n)
    * #backtracking #bit-manipulation #easy
    */
 
   public int subsetXORSum(int[] nums) {
-    return dfs(0, 0, nums);
+    // return dfs(0, 0, nums);
+    int n = nums.length;
+    int xor = 0;
+    for (int i = 0; i < n; i++) {
+      xor |= nums[i];
+    }
+    return xor * (1 << (n - 1)); // xor << (n-1)
   }
 
   private int dfs(int idx, int xor, int[] nums) {
