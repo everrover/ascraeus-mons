704c1561bf7b58fd436c821b6cef3b288acccf8a
everrover <30935508+everrover@users.noreply.github.com>
2025-04-10 00:09:36 +0530

    Love from Africa!

--- dsa/leetcode/VallesMarineris/ImplementRouter.java
+++ dsa/leetcode/VallesMarineris/ImplementRouter.java
@@ -15,52 +15,91 @@ public class ImplementRouter {
      * SC: O(n)
      * #array #hash-table #binary-search #design #queue #medium
      */
-
-    private int memoryLimit;
-    private List<int[]> packetList;
-    private Map<String, Integer> packetMap; // Combining "source-destination-timestamp" as key to handle duplication
-    private Deque<int[]> packetQueue;
-
+    private final static long M = (long)1e6;
+  
+    private Map<Long, Set<Integer>> pacheck = new HashMap<>();
+    private Map<Integer, List<Integer>> pamap = new HashMap<>();
+    private Map<Integer, Integer> old = new HashMap<>();
+    private Queue<int[]> paque = new LinkedList<>();
+  
+    private int ml;
     public ImplementRouter(int memoryLimit) {
-        this.memoryLimit = memoryLimit;
-        packetList = new ArrayList<>();
-        packetMap = new HashMap<>();
-        packetQueue = new ArrayDeque<>();
+      ml = memoryLimit;
     }
-
-    public boolean addPacket(int source, int destination, int timestamp) {
-        String packetKey = source + "-" + destination + "-" + timestamp;
-        if (packetMap.containsKey(packetKey)) {
-            return false; // Duplicate packet
+  
+    private int searchLess(int dest, int ts){
+      if(!pamap.containsKey(dest)) return -1;
+  
+      List<Integer> tss = pamap.get(dest);
+  
+      int l = old.get(dest), r = tss.size()-1, mid = -1;
+      int res = -1;
+      while(l<=r){
+        mid = (l+r)/2;
+        if(tss.get(mid)>ts) r=mid-1;
+        else {
+          l=mid+1;
+          res = mid;
         }
-        if (packetQueue.size() >= memoryLimit) {
-            int[] oldestPacket = packetQueue.poll();
-            String oldestPacketKey = oldestPacket[0] + "-" + oldestPacket[1] + "-" + oldestPacket[2];
-            packetMap.remove(oldestPacketKey);
-        }
-        int[] newPacket = new int[]{source, destination, timestamp};
-        packetQueue.offer(newPacket);
-        packetMap.put(packetKey, packetQueue.size() - 1);
-        return true;
+      }
+  
+      return res;
     }
-
+  
+    private int searchGrt(int dest, int ts){
+      if(!pamap.containsKey(dest)) return -1;
+  
+      List<Integer> tss = pamap.get(dest);
+  
+      int l = old.get(dest), r = tss.size()-1, mid = -1;
+      int res = -1;
+      while(l<=r){
+        mid = (l+r)/2;
+        if(tss.get(mid)>=ts) {
+          r=mid-1;
+          res = mid;
+        } else {
+          l=mid+1;
+        }
+      }
+  
+      return res;
+    }
+    
+    public boolean addPacket(int src, int dest, int ts) {
+      long key = src*M+dest;
+      if(pacheck.containsKey(key) && pacheck.get(key).contains(ts)) return false;
+      if(paque.size() == ml) forwardPacket();
+      
+      pacheck.putIfAbsent(key, new HashSet<>());
+      pacheck.get(key).add(ts);
+  
+      pamap.putIfAbsent(dest, new ArrayList<>());
+      pamap.get(dest).add(ts);
+  
+      old.putIfAbsent(dest, 0);
+      paque.offer(new int[]{src, dest, ts});
+      return true;
+    }
+    
     public int[] forwardPacket() {
-        if (packetQueue.isEmpty()) {
-            return new int[]{}; // No packet to forward
-        }
-        int[] packet = packetQueue.poll();
-        String packetKey = packet[0] + "-" + packet[1] + "-" + packet[2];
-        packetMap.remove(packetKey);
-        return packet;
+      if(paque.isEmpty()) return new int[]{};
+      int[] p = paque.poll();
+      long key = p[0]*M+p[1];
+      if(pacheck.containsKey(key)){
+        if(!pacheck.get(key).contains(p[2])) return new int[]{};
+        pacheck.get(key).remove(p[2]);
+      }
+  
+      old.put(p[1], old.get(p[1])+1);
+      return p;
     }
-
-    public int getCount(int destination, int startTime, int endTime) {
-        int count = 0;
-        for (int[] packet : packetQueue) {
-            if (packet[1] == destination && packet[2] >= startTime && packet[2] <= endTime) {
-                count++;
-            }
-        }
-        return count;
+    
+    public int getCount(int dest, int st, int en) {
+      int sidx = searchGrt(dest, st);
+      int eidx = searchLess(dest, en);
+      if(sidx>eidx) return 0;
+      else if(sidx == -1 || eidx == -1) return 0;
+      else return eidx-sidx+1;
     }
 }
