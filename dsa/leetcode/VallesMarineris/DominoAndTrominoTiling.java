7f02489452a1ed26e3bce0fb6425795084f8f489
everrover <30935508+everrover@users.noreply.github.com>
2025-05-05 21:41:20 +0530

    added mods \

--- dsa/leetcode/VallesMarineris/DominoAndTrominoTiling.java
+++ dsa/leetcode/VallesMarineris/DominoAndTrominoTiling.java
@@ -1,30 +1,51 @@
 package dsa.leetcode.VallesMarineris;
 
+import java.util.Arrays;
+
 public class DominoAndTrominoTiling {
 
   /**
    * https://leetcode.com/problems/domino-and-tromino-tiling/description/?envType=daily-question&envId=2025-05-05
    *
-   * This function calculates the number of ways to tile a 2 x n board with dominoes and trominoes using dynamic programming.
-   * It uses two arrays to store subproblem solutions and computes the result in a modular arithmetic to handle large numbers.
+   * IF we use a domino, we can place it in two ways: |(add 1) or =(add 2)
+   * IF we use a tromino, we can place it in two ways: L(add 2) or it's diagonal mirror (add 2)
+   * 
+   * Using the dfs approach below, we place either one domino or one tromino at each step. That's why partial case is like that.
+   * Because when it's called, we already placed one tromino and need to place either one domino at `idx-2` or one tromino at `idx-1`.
    *
    * TC: O(n), SC: O(n)
    * #dynamic-programming #tiling #medium
    */
 
-  private long dfs_p(int idx, final long[] dpf, final long[] dpp, final int n) {
-    if (idx < 0) return 0;
-    if (dpp[idx] != -1) return dpp[idx];
-    long res = 0;
-    if (idx == 2) res = 1L;
-    else if (idx == 2) res = 2L;
-    if (idx == 1) res = 1L;
-
-    res = (dfs_f(idx - 1, dpf, dpp, n) +
-           dfs_f(idx - 2, dpf, dpp, n) +
-           2 * dfs_p(idx - 1, dpf, dpp, n) +
-           res) % MOD;
-
-    return dpp[idx] = res;
+  final int MOD = (int)1e9+7;
+  public int numTilings(int n) {
+    int res = 0;
+    long []dpf = new long[1001];
+    long []dpp = new long[1001];
+    Arrays.fill(dpf, -1);
+    Arrays.fill(dpp, -1);
+    return (int)dfs_f(n, dpf, dpp, n);
   }
+  private long dfs_f(int idx, long []dpf, long []dpp, final int n){
+    if(idx < 0) return 0;
+    if(dpf[idx] != -1) return dpf[idx]; 
+    long res = 0;
+    if(idx == 1) res = 1L;
+    else if(idx == 2) res = 2L;
+    else res = (
+      res + 
+      dfs_f(idx-1, dpf, dpp, n) + 
+      dfs_f(idx-2, dpf, dpp, n) + 
+      2 * dfs_p(idx-1, dpf, dpp, n)
+    ) % MOD;
+    return dpf[idx] = res;
+  }
+  private long dfs_p(int idx, final long []dpf, final long []dpp, final int n){
+    if(idx < 0) return 0;
+    if(dpp[idx] != -1) return dpp[idx]; 
+    long res = 0;
+    if(idx == 2) res = 1L;
+    else res = (res + dfs_p(idx-1, dpf, dpp, n) + dfs_f(idx-2, dpf, dpp, n)) % MOD;
+    return dpp[idx] = res;
+  } 
 }
