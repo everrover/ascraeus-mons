5a515f75c5f0e7c6505dc8860d8ce887bee2f9f7
everrover <30935508+everrover@users.noreply.github.com>
2025-07-06 23:41:32 +0530

    added mods

--- dsa/leetcode/Ipestus/MinimumDeletionsToMakeStringKSpecial.java
+++ dsa/leetcode/Ipestus/MinimumDeletionsToMakeStringKSpecial.java
@@ -1,14 +1,14 @@
 package dsa.leetcode.Ipestus;
 
-class Solution {
+class MinimumDeletionsToMakeStringKSpecial {
 
     /**
      * https://leetcode.com/problems/minimum-deletions-to-make-string-k-special/description/?envType=daily-question&envId=2025-06-21
      *
-     * To make a string k-special, compete by minimizing deletions such
-     * that the frequency difference between any two characters does not
-     * exceed k. Count each character's frequency, assume a certain frequency
-     * to be the minimal allowed, and then adjust accordingly.
+     * Trick: Try assuming one of the characters to be the smallest. Say, `x`
+     *
+     * Then for each other character `y`, if the freq is lesser than `x` then we attempt deleting `y` completely.
+     * Else if abs(freq[y] - freq[x]) > k, then we delete the excess characters of `y` to make it equal to `x + k`.
      *
      * TC: O(n) SC: O(26)
      * #hash-table #string #greedy #medium
@@ -20,9 +20,15 @@ class Solution {
         int res = chs.length;
         for (char ch : chs) freq[ch - 'a']++;
         for (int i = 0; i < 26; i++) {
-            if (freq[i] > freq[j]) {
-                del += freq[j];
+            int del = 0;
+            for(int j=0; j<26; j++){
+                if(freq[i]>freq[j]){
+                    del += freq[j];
+                }else if(freq[j] > freq[i]+k) {
+                    del += freq[j] - (freq[i]+k);
+                }
             }
+            res = Math.min(res, del);
         }
         return res;
     }
