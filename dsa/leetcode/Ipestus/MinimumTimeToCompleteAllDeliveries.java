da4d1922d9392ab18d8424c30c39a6967f65a7b3
everrover <30935508+everrover@users.noreply.github.com>
2026-01-31 23:32:58 +0530

    added mods

--- dsa/leetcode/Ipestus/MinimumTimeToCompleteAllDeliveries.java
+++ dsa/leetcode/Ipestus/MinimumTimeToCompleteAllDeliveries.java
@@ -5,10 +5,19 @@ public class MinimumTimeToCompleteAllDeliveries {
   /**
    * https://leetcode.com/problems/minimum-time-to-complete-all-deliveries/description/
    *
-   * Use binary search to find the minimum total time required for all drone deliveries,
-   * accounting for each drone's recharge interval. Consider effective working time
-   * and shared hours where neither drone can work.
-   *
+   * Use binary search over the solution space of time to find the minimum time.
+   * 
+   * Recharge time exclusively for drone 1 => floor(time / r1)
+   * similarly for drone 2 => floor(time / r2)
+   * Also, shared time when both drones are not delivering = floor(time / lcm(r1, r2))
+   * So shared available time = time - (time / r1) - (time / r2) + (time / lcm(r1, r2)) // inclusion-exclusion
+   * 
+   * For a given mid time, we can calculate the maximum deliveries each drone can make:
+   * drone 1 => ex1 = mid - floor(mid / r1) >= d1
+   * drone 2 => ex2 = mid - floor(mid / r2) >= d2
+   * combined deliveries => mid - floor(mid / lcm(r1, r2)) >= d1 + d2
+   *                     => mid - ex1 - ex2 + shared >= d1 + d2
+   * 
    * TC: O(log(max_deliveries)) SC: O(1)
    * #binary-search #math #medium
    */
