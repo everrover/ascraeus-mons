b73a0a6b6dd97b2c90fd1ca0f250155fa7f758be
everrover <30935508+everrover@users.noreply.github.com>
2026-02-08 13:40:19 +0530

    added mods to the following files:

--- dsa/leetcode/Ipestus/MinimumOperationsToWriteTheLetterYOnAGrid.java
+++ dsa/leetcode/Ipestus/MinimumOperationsToWriteTheLetterYOnAGrid.java
@@ -5,33 +5,31 @@ public class MinimumOperationsToWriteTheLetterYOnAGrid {
   /**
    * https://leetcode.com/problems/minimum-operations-to-write-the-letter-y-on-a-grid/description/
    *
-   * Calculate minimum changes to shape the grid into a 'Y'.
    * Strategy involves counting operations needed to modify to Y and not-Y areas.
    *
    * TC: O(n^2) SC: O(1)
    * #matrix #array #medium
    */
 
-  public int minOperations(int[][] grid) {
-    int N = grid.length;
-    int[] y = new int[3]; // Counter for the 'Y' pattern
-    int[] body = new int[3]; // Counter for the rest
-    
-    for(int r = 0; r < N; r++) {
-      for(int c = 0; c < N; c++) body[grid[r][c]]++;
-    }
-
-    // Count for the center vertical line
-    for(int r = N/2, c = N/2; r < N; r++) y[grid[r][c]]++;
-
-    // Calculating result
+  public int minimumOperationsToWriteY(int[][] grid) {
+    final int N = grid.length;
+    int res = Integer.MAX_VALUE;
+    int[] y = new int[3], body = new int[3]; 
+    // main diagonal
+    for(int r=0,c=0; r<N/2; r++, c++) y[grid[r][c]]++;
+    // sub diagonal
+    for(int r=0,c=N-1; r<N/2; r++, c--) y[grid[r][c]]++;
+    // line
+    for(int r=N/2,c=N/2; r<N; r++) y[grid[r][c]]++;
+    // body
+    for(int r=0; r<N; r++) for(int c=0; c<N; c++) body[grid[r][c]]++;
     body[0] -= y[0];
     body[1] -= y[1];
     body[2] -= y[2];
-    int res = Integer.MAX_VALUE;
-    for(int i = 0; i < 3; i++) {
-      res = Math.min(res,
-        y[(i+1)%3] + y[(i+2)%3] + body[i] + Math.min(
+
+    for(int i=0; i<3; i++){
+      res = Math.min(res, 
+        y[(i+1)%3]+y[(i+2)%3]+body[i]+Math.min(
           body[(i+1)%3], body[(i+2)%3]
         )
       );
